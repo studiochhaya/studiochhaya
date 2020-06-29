dd6a841a7f06a39e772dafd09c0bd4d74bbb05a1
Paresh Chhatrala <paresh.chhatrala@gmail.com>
2020-06-28 22:15:34 -0500

    Adding angular files.

---
+++ app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavMenuComponent
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
